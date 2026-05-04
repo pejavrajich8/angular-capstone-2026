7fc8f8127c861d053e6d3cf06d3294e230948758
Seth Tomlinson <155940170+SethT3345@users.noreply.github.com>
2026-05-04 08:14:54 -0600

    got rid of hid tabs button becuase it was trash

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { Tab1Page } from './tab1/tab1.page';
+import { Tab2Page } from './poker/tab2.page';
+import { Tab3Page } from './tab3/tab3.page';
+import { Tab4Page } from './tab4/tab4.page';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then(m => m.routes),
+  },
   {
     path: 'tabs',
     children: [
@@ -9,7 +16,18 @@ export const routes: Routes = [
         path: 'tab1',
         component: Tab1Page,
       },
-      // ...other tabs
+      {
+        path: 'poker',
+        component: Tab2Page,
+      },
+      {
+        path: 'tab3',
+        component: Tab3Page,
+      },
+      {
+        path: 'tab4',
+        component: Tab4Page,
+      },
     ],
   },
 ];
